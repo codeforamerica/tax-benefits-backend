b61876c33d395ceff08e252913b54a4728c66603
James Armes <jarmes@codeforamerica.org>
2024-10-21 12:39:23 -0400

    feat: Added configuration for demo.getyourrefund.org.

---
+++ tofu/config/demo.getyourrefund.org/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "s3" {
+    bucket = "tax-benefits-prod-tfstate"
+    key    = "demo.getyourrefund.org.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "prod.tfstate"
+  }
+}
+
+module "logging" {
+  source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=1.1.0"
+
+  project                  = "gyr"
+  environment              = "demo"
+  cloudwatch_log_retention = 30
+  log_groups = {
+    "waf" = {
+      name = "aws-waf-logs-cfa/gyr/demo"
+      tags = {
+        source = "waf"
+        webacl = "gyr-demo"
+        domain = "demo.getyourrefund.org"
+      }
+    }
+  }
+}
+
+module "waf" {
+  source = "../../modules/aptible_waf"
+
+  project                 = "gyr"
+  environment             = "demo"
+  domain                  = "getyourrefund.org"
+  log_bucket              = module.logging.bucket_domain_name
+  log_group               = module.logging.log_groups["waf"]
+  aptible_environment     = "vita-min-demo"
+  aptible_app_id          = 17865
+  allow_security_scanners = true
+  passive                 = true
+}
