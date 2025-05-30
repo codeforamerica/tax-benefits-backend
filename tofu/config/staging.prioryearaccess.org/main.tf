800120d1ee511e92b982acd5051ac60945e7b322
Arin Choi <achoi@codeforamerica.org>
2025-05-30 12:01:22 -0700

    Troubleshooting with James - fixing s3, enabling db & web, creating outputs for easy access to things we need for deploy

--- tofu/config/staging.prioryearaccess.org/main.tf
+++ tofu/config/staging.prioryearaccess.org/main.tf
@@ -1,11 +1,18 @@
 terraform {
   backend "s3" {
-    bucket         = "tax-benefits-staging-pya"
+    bucket         = "pya-staging-tfstate"
     key            = "staging.prioryearaccess.org"
     region         = "us-east-1"
+    dynamodb_table = "staging.tfstate"
   }
 }
 
+module "backend" {
+  source = "github.com/codeforamerica/tofu-modules-aws-backend?ref=1.1.1"
+
+  project     = "pya"
+  environment = "staging"
+}
 
 module "logging" {
   source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.0"
@@ -46,42 +53,43 @@ module "secrets" {
 #
 
 
-# module "vpc" {
-#   source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
-#
-#   project        = "pya"
-#   environment    = "staging"
-#   cidr           = "10.0.20.0/22"
-#   logging_key_id = module.logging.kms_key_arn
-#
-#   private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
-#   public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
-# }
-#
-# module "web" {
-#   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-#
-#   project       = "pya"
-#   project_short = "pya"
-#   environment   = "staging"
-#   service       = "web"
-#   service_short = "web"
-#
-#   domain          = "staging.prioryearaccess.org"
-#   vpc_id          = module.vpc.vpc_id
-#   private_subnets = module.vpc.private_subnets
-#   public_subnets  = module.vpc.public_subnets
-#   logging_key_id  = module.logging.kms_key_arn
-#   container_port  = 3000
-#   create_endpoint	= true
-#   create_repository	= true
-#   create_version_parameter = true
-#
-#   environment_variables = {
-#     RACK_ENV = "staging"
-#   }
-# }
-#
+module "vpc" {
+  source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+
+  project        = "pya"
+  environment    = "staging"
+  cidr           = "10.0.20.0/22"
+  logging_key_id = module.logging.kms_key_arn
+
+  private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
+  public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
+}
+
+module "web" {
+  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+
+  project       = "pya"
+  project_short = "pya"
+  environment   = "staging"
+  service       = "web"
+  service_short = "web"
+
+  domain          = "staging.prioryearaccess.cfa.codes"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  logging_key_id  = module.logging.kms_key_arn
+  container_port  = 3000
+  create_endpoint	= true
+  create_repository	= true
+  create_version_parameter = true
+  public = true
+
+  environment_variables = {
+    RACK_ENV = "staging"
+  }
+}
+
 # module "workers" {
 #   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
 #
@@ -91,33 +99,34 @@ module "secrets" {
 #   service       = "worker"
 #   service_short = "wrk"
 #
-#   domain          = "staging.prioryearaccess.org"
 #   vpc_id          = module.vpc.vpc_id
 #   private_subnets = module.vpc.private_subnets
 #   public_subnets  = module.vpc.public_subnets
 #   logging_key_id  = module.logging.kms_key_arn
 #   container_port  = 3000
+#   version_parameter = module.web.version_parameter
+#   image_url = module.web.repository_url
 #
 #   environment_variables = {
 #     RACK_ENV = "staging"
 #   }
 # }
-#
-#
-# module "database" {
-#   source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=1.2.0"
-#
-#   project     = "pya"
-#   environment = "staging"
-#   service     = "web"
-#
-#   logging_key_arn = module.logging.kms_key_arn
-#   secrets_key_arn = module.secrets.kms_key_arn
-#   vpc_id          = module.vpc.vpc_id
-#   subnets         = module.vpc.private_subnets
-#   ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
-#
-#   min_capacity = 2
-#   max_capacity = 32
-#   cluster_parameters = []
-# }
+
+module "database" {
+  source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=log-exports"
+
+  project     = "pya"
+  environment = "staging"
+  service     = "web"
+
+  logging_key_arn = module.logging.kms_key_arn
+  secrets_key_arn = module.secrets.kms_key_arn
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+  ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
+  iam_authentication = false
+
+  min_capacity = 2
+  max_capacity = 32
+  cluster_parameters = []
+}
--- tofu/config/staging.prioryearaccess.org/outputs.tf
+++ tofu/config/staging.prioryearaccess.org/outputs.tf
@@ -2,3 +2,16 @@ output "log_bucket" {
   description = "The Amazon S3 bucket to store log files."
   value       = module.logging.bucket
 }
+
+# Display commands to push the Docker image to ECR.
+output "docker_push" {
+  value = module.web.docker_push
+}
+
+output "repository_arn" {
+  value = module.web.repository_arn
+}
+
+output "repository_url" {
+  value = module.web.repository_url
+}
