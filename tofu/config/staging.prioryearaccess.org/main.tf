c2a78bdc10df6e3dc017fde718982e06027a1536
Arin Choi <achoi@codeforamerica.org>
2025-05-29 22:23:19 -0700

    Add logging, secrets, waf, web/worker fargate modules

--- tofu/config/staging.prioryearaccess.org/main.tf
+++ tofu/config/staging.prioryearaccess.org/main.tf
@@ -7,44 +7,79 @@ terraform {
   }
 }
 
-#
-# module "logging" {
-#   source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.0"
-#
-#   project                  = "pya"
-#   environment              = "staging"
-#   cloudwatch_log_retention = 30
-#   log_groups = {
-#     "waf" = {
-#       name = "aws-waf-logs-cfa/fyst/staging"
-#       tags = {
-#         source = "waf"
-#         webacl = "fyst-staging"
-#         domain = "staging.fileyourstatetaxes.org"
-#       }
-#     }
-#   }
-# }
-#
-# # We don't need to create any secrets here, but we need the infrastructure for
-# # other modules to utilize.
-# module "secrets" {
-#   source = "github.com/codeforamerica/tofu-modules-aws-secrets?ref=1.0.0"
-#
-#   project     = "fyst"
-#   environment = "staging"
-# }
-#
-# module "waf" {
-#   source = "../../modules/aptible_waf"
-#
-#   project              = "fyst"
-#   environment          = "staging"
-#   domain               = "fileyourstatetaxes.org"
-#   log_bucket           = module.logging.bucket_domain_name
-#   log_group            = module.logging.log_groups["waf"]
-#   aptible_environment  = "vita-min-staging"
-#   aptible_app_id       = 17866
-#   allow_security_scans = false
-#   secrets_key_arn      = module.secrets.kms_key_arn
-# }
+
+module "logging" {
+  source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.0"
+
+  project                  = "pya"
+  environment              = "staging"
+  cloudwatch_log_retention = 30
+  log_groups = {
+    "waf" = {
+      name = "aws-waf-logs-cfa/fyst/staging"
+      tags = {
+        source = "waf"
+        webacl = "pya-staging"
+        domain = "staging.prioryearaccess.org"
+      }
+    }
+  }
+}
+
+# We don't need to create any secrets here, but we need the infrastructure for
+# other modules to utilize.
+module "secrets" {
+  source = "github.com/codeforamerica/tofu-modules-aws-secrets?ref=1.0.0"
+
+  project     = "pya"
+  environment = "staging"
+}
+
+module "cloudfront_waf" {
+  source = "github.com/codeforamerica/tofu-modules-aws-cloudfront-waf?ref=1.9.0"
+
+  project     = "pya"
+  environment = "staging"
+  domain      = "staging.prioryearaccess.org"
+  log_bucket  = module.logging.bucket
+}
+
+module "web" {
+  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+
+  project       = "pya"
+  environment   = "staging"
+  service       = "web"
+  service_short = "web"
+
+  domain          = "staging.prioryearaccess.org"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  logging_key_id  = module.logging.kms_key_arn
+  container_port  = 3000
+
+  environment_variables = {
+    RACK_ENV = "staging"
+  }
+}
+
+module "workers" {
+  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+
+  project       = "pya"
+  environment   = "staging"
+  service       = "worker"
+  service_short = "wrk"
+
+  domain          = "staging.prioryearaccess.org"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  logging_key_id  = module.logging.kms_key_arn
+  container_port  = 3000
+
+  environment_variables = {
+    RACK_ENV = "staging"
+  }
+}
