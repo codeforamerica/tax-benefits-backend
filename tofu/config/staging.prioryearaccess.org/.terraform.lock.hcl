6c3adfe4b0a8e7960fefaf549cbf29c82779d717
Arin Choi <achoi@codeforamerica.org>
2025-05-30 09:14:43 -0700

    Commented out fargate / db stuff to allow tofu apply to run locally

---
+++ tofu/config/staging.prioryearaccess.org/.terraform.lock.hcl
@@ -0,0 +1,58 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/aptible/aptible" {
+  version     = "0.9.13"
+  constraints = "~> 0.9"
+  hashes = [
+    "h1:2Smprh/xZAfd/JyFTsviVhpvXOE56WCMq13iYbfA2ww=",
+    "zh:0c0788aee0b543d1e9d0f06f485b4379d6681d1064aad55628b69f94612855da",
+    "zh:1c40e6726c000193acde16d1d4253f6831a771badd24a84af8fd5a69b971dc03",
+    "zh:25b33e2037ef97400f90bb9d9d2a28a723a5a05694c7fac339f6c6ced06fb2e8",
+    "zh:662e052089d14e095611417cc5c80822ffca60b07fb03776c5cd858754698589",
+    "zh:70fa227b8441df6568c7134275c81813d002bc6b4d7bd4c54493d23627df86dd",
+    "zh:b5982bb0014f50d172c8fa0415e2c8956a71f20fed7920797f0e87ebcb716f7f",
+    "zh:b97557195a02aa1603957c782784869cead3f6ba991c4a24d95a2627fd9e0d7e",
+    "zh:cb5e7b889eba7c1a855be9482ae0c7f51281132c1a93f683fb38957546a95e4d",
+    "zh:d8a02e684c42a9c3a040f10bfc74626b1ab7f26a8f3f7c938a735ea9c357a9f8",
+    "zh:d92cb16f3965963edcbbdd7970f8ee01832cf12cd808665a8e0eb3c0f8eaf641",
+    "zh:ddd8d4302a6da5fc05c2b4552a5eedbb7d266ce61a43569e31aad6ba96a0edd6",
+    "zh:f8eaef88893ec7bf5dbefc8f27a321eb4b600cbc4372198909a9c2c20feffc34",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/aws" {
+  version     = "5.99.0"
+  constraints = ">= 5.44.0, ~> 5.44"
+  hashes = [
+    "h1:r3jPU1mwOZfJWHZ7HtvH2w5tHMgiQMyBuSzbkm3GMx4=",
+    "zh:08cde495f6580aab13299196de8e833038d05eb0a09d754c1d09b74b842635e4",
+    "zh:3eeec5ad1ed4221edc8c7f177227b24efb3fb3db75b848bf6d52aeaf82ae037c",
+    "zh:4f52c4ee689fea5a4ead15d951bfff0ad296ec7c1968644ee55573ece983abc0",
+    "zh:927443998b1afcb47cc97a89cbe804937e7843f276d3c86fcdd78606f8bd1965",
+    "zh:ae216473734c5ac00d48dde15d8cebd0d281d4bf9ee32b9bd20619a53a8b0499",
+    "zh:b1209d974b4d03fd88ebe3e2d653707851f85cd3719176e0de94d7e5a5a7fcbf",
+    "zh:bd71aedc9aacb3089de8d3dacba6b68ca4da8a8169ab801a9d6458b8ac65ffc9",
+    "zh:ea3a7ddf11114e792c64bb53cca3cc7d508ca8f569d178446428d5f44b30df5b",
+    "zh:ebce07acd223dcd34c91aa8f73d52535f9081e587603056170a87657be45c191",
+    "zh:f9f6b479c8d1b96dde159d8b14fcbaa1e865b0ae9d7661a4b9bc6bf7a75d44bf",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/random" {
+  version     = "3.7.2"
+  constraints = ">= 3.0.0"
+  hashes = [
+    "h1:cFGCdxTlsrteTiaOV/iOQdql7eJkD3F/vtJxenkj9IE=",
+    "zh:2ffeb1058bd7b21a9e15a5301abb863053a2d42dffa3f6cf654a1667e10f4727",
+    "zh:519319ed8f4312ed76519652ad6cd9f98bc75cf4ec7990a5684c072cf5dd0a5d",
+    "zh:7371c2cc28c94deb9dba62fbac2685f7dde47f93019273a758dd5a2794f72919",
+    "zh:9b0ac4c1d8e36a86b59ced94fa517ae9b015b1d044b3455465cc6f0eab70915d",
+    "zh:c6336d7196f1318e1cbb120b3de8426ce43d4cacd2c75f45dba2dbdba666ce00",
+    "zh:c71f18b0cb5d55a103ea81e346fb56db15b144459123f1be1b0209cffc1deb4e",
+    "zh:d2dc49a6cac2d156e91b0506d6d756809e36bf390844a187f305094336d3e8d8",
+    "zh:d5b5fc881ccc41b268f952dae303501d6ec9f9d24ee11fe2fa56eed7478e15d0",
+    "zh:db9723eaca26d58c930e13fde221d93501529a5cd036b1f167ef8cff6f1a03cc",
+    "zh:fe3359f733f3ab518c6f85f3a9cd89322a7143463263f30321de0973a52d4ad8",
+  ]
+}
--- tofu/config/staging.prioryearaccess.org/main.tf
+++ tofu/config/staging.prioryearaccess.org/main.tf
@@ -46,78 +46,78 @@ module "secrets" {
 #
 
 
-module "vpc" {
-  source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
-
-  project        = "pya"
-  environment    = "staging"
-  cidr           = "10.0.20.0/22"
-  logging_key_id = module.logging.kms_key_arn
-
-  private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
-  public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
-}
-
-module "web" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "web"
-  service_short = "web"
-
-  domain          = "staging.prioryearaccess.org"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  create_endpoint	= true
-  create_repository	= true
-  create_version_parameter = true
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-}
-
-module "workers" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "worker"
-  service_short = "wrk"
-
-  domain          = "staging.prioryearaccess.org"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-}
-
-
-module "database" {
-  source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=1.2.0"
-
-  project     = "pya"
-  environment = "staging"
-  service     = "web"
-
-  logging_key_arn = module.logging.kms_key_arn
-  secrets_key_arn = module.secrets.kms_key_arn
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
-  ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
-
-  min_capacity = 2
-  max_capacity = 32
-  cluster_parameters = []
-}
+# module "vpc" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+#
+#   project        = "pya"
+#   environment    = "staging"
+#   cidr           = "10.0.20.0/22"
+#   logging_key_id = module.logging.kms_key_arn
+#
+#   private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
+#   public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
+# }
+#
+# module "web" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+#
+#   project       = "pya"
+#   project_short = "pya"
+#   environment   = "staging"
+#   service       = "web"
+#   service_short = "web"
+#
+#   domain          = "staging.prioryearaccess.org"
+#   vpc_id          = module.vpc.vpc_id
+#   private_subnets = module.vpc.private_subnets
+#   public_subnets  = module.vpc.public_subnets
+#   logging_key_id  = module.logging.kms_key_arn
+#   container_port  = 3000
+#   create_endpoint	= true
+#   create_repository	= true
+#   create_version_parameter = true
+#
+#   environment_variables = {
+#     RACK_ENV = "staging"
+#   }
+# }
+#
+# module "workers" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+#
+#   project       = "pya"
+#   project_short = "pya"
+#   environment   = "staging"
+#   service       = "worker"
+#   service_short = "wrk"
+#
+#   domain          = "staging.prioryearaccess.org"
+#   vpc_id          = module.vpc.vpc_id
+#   private_subnets = module.vpc.private_subnets
+#   public_subnets  = module.vpc.public_subnets
+#   logging_key_id  = module.logging.kms_key_arn
+#   container_port  = 3000
+#
+#   environment_variables = {
+#     RACK_ENV = "staging"
+#   }
+# }
+#
+#
+# module "database" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=1.2.0"
+#
+#   project     = "pya"
+#   environment = "staging"
+#   service     = "web"
+#
+#   logging_key_arn = module.logging.kms_key_arn
+#   secrets_key_arn = module.secrets.kms_key_arn
+#   vpc_id          = module.vpc.vpc_id
+#   subnets         = module.vpc.private_subnets
+#   ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
+#
+#   min_capacity = 2
+#   max_capacity = 32
+#   cluster_parameters = []
+# }
