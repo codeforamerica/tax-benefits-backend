c3ffd3ba72a5b8aff10bf6ec75b403bbd88fc5e8
Arin Choi <achoi@codeforamerica.org>
2025-06-02 09:51:19 -0700

    Update domain to the newly provisioned one and comment out VPC-related configs to destroy them since VPC subnets cannot be replaced easily

--- tofu/config/staging.prioryearaccess.org/main.tf
+++ tofu/config/staging.prioryearaccess.org/main.tf
@@ -26,7 +26,7 @@ module "logging" {
       tags = {
         source = "waf"
         webacl = "pya-staging"
-        domain = "staging.prioryearaccess.org"
+        domain = "staging.pya.fileyourstatetaxes.org"
       }
     }
   }
@@ -46,88 +46,88 @@ module "secrets" {
 #
 #   project     = "pya"
 #   environment = "staging"
-#   domain      = "staging.prioryearaccess.org"
+#   domain      = "staging.pya.fileyourstatetaxes.org"
 #   log_bucket  = module.logging.bucket
 #   /*  The argument "log_group" is required, but no definition was found. What should this be? */
 # }
 #
 
 
-module "vpc" {
-  source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+# module "vpc" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+#
+#   project        = "pya"
+#   environment    = "staging"
+#   cidr           = "10.0.36.0/22"
+#   logging_key_id = module.logging.kms_key_arn
+#
+#   private_subnets = ["10.0.38.0/26", "10.0.38.64/26", "10.0.38.128/26"]
+#   public_subnets  = ["10.0.36.0/26", "10.0.36.64/26", "10.0.36.128/26"]
+# }
 
-  project        = "pya"
-  environment    = "staging"
-  cidr           = "10.0.20.0/22"
-  logging_key_id = module.logging.kms_key_arn
-
-  private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
-  public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
-}
-
-module "web" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "web"
-  service_short = "web"
-
-  domain          = "staging.prioryearaccess.cfa.codes"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  create_endpoint	= true
-  create_repository	= true
-  create_version_parameter = true
-  public = true
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-}
-
-module "workers" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "worker"
-  service_short = "wrk"
-
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  version_parameter = module.web.version_parameter
-  image_url = module.web.repository_url
-  create_endpoint = false
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-}
-
-module "database" {
-  source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=log-exports"
-
-  project     = "pya"
-  environment = "staging"
-  service     = "web"
-
-  logging_key_arn = module.logging.kms_key_arn
-  secrets_key_arn = module.secrets.kms_key_arn
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
-  ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
-  iam_authentication = false
-
-  min_capacity = 2
-  max_capacity = 32
-  cluster_parameters = []
-}
+# module "web" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+#
+#   project       = "pya"
+#   project_short = "pya"
+#   environment   = "staging"
+#   service       = "web"
+#   service_short = "web"
+#
+#   domain          = "staging.pya.fileyourstatetaxes.org"
+#   vpc_id          = module.vpc.vpc_id
+#   private_subnets = module.vpc.private_subnets
+#   public_subnets  = module.vpc.public_subnets
+#   logging_key_id  = module.logging.kms_key_arn
+#   container_port  = 3000
+#   create_endpoint	= true
+#   create_repository	= true
+#   create_version_parameter = true
+#   public = true
+#
+#   environment_variables = {
+#     RACK_ENV = "staging"
+#   }
+# }
+#
+# module "workers" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
+#
+#   project       = "pya"
+#   project_short = "pya"
+#   environment   = "staging"
+#   service       = "worker"
+#   service_short = "wrk"
+#
+#   vpc_id          = module.vpc.vpc_id
+#   private_subnets = module.vpc.private_subnets
+#   public_subnets  = module.vpc.public_subnets
+#   logging_key_id  = module.logging.kms_key_arn
+#   container_port  = 3000
+#   version_parameter = module.web.version_parameter
+#   image_url = module.web.repository_url
+#   create_endpoint = false
+#
+#   environment_variables = {
+#     RACK_ENV = "staging"
+#   }
+# }
+#
+# module "database" {
+#   source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=log-exports"
+#
+#   project     = "pya"
+#   environment = "staging"
+#   service     = "web"
+#
+#   logging_key_arn = module.logging.kms_key_arn
+#   secrets_key_arn = module.secrets.kms_key_arn
+#   vpc_id          = module.vpc.vpc_id
+#   subnets         = module.vpc.private_subnets
+#   ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
+#   iam_authentication = false
+#
+#   min_capacity = 2
+#   max_capacity = 32
+#   cluster_parameters = []
+# }
--- tofu/config/staging.prioryearaccess.org/outputs.tf
+++ tofu/config/staging.prioryearaccess.org/outputs.tf
@@ -3,17 +3,17 @@ output "log_bucket" {
   value       = module.logging.bucket
 }
 
-output "docker_push" {
-  description = "Display commands to push the Docker image to ECR."
-  value = module.web.docker_push
-}
-
-output "repository_arn" {
-  description = "AWS Repository Amazon Resource Name (ARN) for ECR"
-  value = module.web.repository_arn
-}
-
-output "repository_url" {
-  description = "AWS Repository URL for ECR"
-  value = module.web.repository_url
-}
+# output "docker_push" {
+#   description = "Display commands to push the Docker image to ECR."
+#   value = module.web.docker_push
+# }
+#
+# output "repository_arn" {
+#   description = "AWS Repository Amazon Resource Name (ARN) for ECR"
+#   value = module.web.repository_arn
+# }
+#
+# output "repository_url" {
+#   description = "AWS Repository URL for ECR"
+#   value = module.web.repository_url
+# }
