d0c6ecfe509020387f540f39e096f7f7c38a8b98
Yejee Arin Choi <yej.arin.choi@gmail.com>
2025-06-12 11:54:44 -0700

    Create pya module (#33)

--- tofu/config/staging.pya.fileyourstatetaxes.org/main.tf
+++ tofu/config/staging.pya.fileyourstatetaxes.org/main.tf
@@ -14,42 +14,6 @@ module "backend" {
   environment = "staging"
 }
 
-module "logging" {
-  source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.0"
-
-  project                  = "pya"
-  environment              = "staging"
-  cloudwatch_log_retention = 30
-  log_groups = {
-    "waf" = {
-      name = "aws-waf-logs-cfa/pya/staging"
-      tags = {
-        source = "waf"
-        webacl = "pya-staging"
-        domain = "staging.pya.fileyourstatetaxes.org"
-      }
-    }
-  }
-}
-
-# We don't need to create any secrets here, but we need the infrastructure for
-# other modules to utilize.
-module "secrets" {
-  source = "github.com/codeforamerica/tofu-modules-aws-secrets?ref=1.0.0"
-
-  project     = "pya"
-  environment = "staging"
-
-  secrets = {
-    "rails_secret_key_base" = {
-      description = "secret_key_base for Rails app"
-      start_value = jsonencode({
-        key = ""
-      })
-    }
-  }
-}
-
 # module "cloudfront_waf" {
 #   source = "github.com/codeforamerica/tofu-modules-aws-cloudfront-waf?ref=1.9.0"
 #
@@ -61,86 +25,12 @@ module "secrets" {
 # }
 #
 
-module "vpc" {
-  source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+module "pya" {
+  source = "../../modules/pya"
 
-  project        = "pya"
-  environment    = "staging"
-  cidr           = "10.0.36.0/22"
-  logging_key_id = module.logging.kms_key_arn
-
-  private_subnets = ["10.0.38.0/26", "10.0.38.64/26", "10.0.38.128/26"]
-  public_subnets  = ["10.0.36.0/26", "10.0.36.64/26", "10.0.36.128/26"]
-}
-
-module "web" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "web"
-  service_short = "web"
-
-  domain          = "staging.pya.fileyourstatetaxes.org"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  create_endpoint	= true
-  create_repository	= true
-  create_version_parameter = true
-  public = true
-  health_check_path = "/up"
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-  environment_secrets = {
-    SECRET_KEY_BASE = "${module.secrets.secrets["rails_secret_key_base"].secret_arn}:key"
-  }
-}
-
-module "workers" {
-  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.2.0"
-
-  project       = "pya"
-  project_short = "pya"
-  environment   = "staging"
-  service       = "worker"
-  service_short = "wrk"
-
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  version_parameter = module.web.version_parameter
-  image_url = module.web.repository_url
-  create_endpoint = false
-
-  environment_variables = {
-    RACK_ENV = "staging"
-  }
-}
-
-module "database" {
-  source = "github.com/codeforamerica/tofu-modules-aws-serverless-database?ref=log-exports"
-
-  project     = "pya"
-  environment = "staging"
-  service     = "web"
-  skip_final_snapshot	= true
-
-  logging_key_arn = module.logging.kms_key_arn
-  secrets_key_arn = module.secrets.kms_key_arn
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
-  ingress_cidrs   = module.vpc.private_subnets_cidr_blocks
-  iam_authentication = false
-
-  min_capacity = 2
-  max_capacity = 32
-  cluster_parameters = []
+  environment         = "staging"
+  domain              = "staging.pya.fileyourstatetaxes.org"
+  cidr                = "10.0.36.0/22"
+  private_subnets     = ["10.0.38.0/26", "10.0.38.64/26", "10.0.38.128/26"]
+  public_subnets      = ["10.0.36.0/26", "10.0.36.64/26", "10.0.36.128/26"]
 }
--- tofu/modules/pya/outputs.tf
+++ tofu/modules/pya/outputs.tf
@@ -1,19 +1,19 @@
 output "log_bucket" {
   description = "The Amazon S3 bucket to store log files."
-  value       = module.pya.log_bucket
+  value       = module.logging.bucket
 }
 
 output "docker_push" {
   description = "Display commands to push the Docker image to ECR."
-  value = module.pya.docker_push
+  value = module.web.docker_push
 }
 
 output "repository_arn" {
   description = "AWS Repository Amazon Resource Name (ARN) for ECR"
-  value = module.pya.repository_arn
+  value = module.web.repository_arn
 }
 
 output "repository_url" {
   description = "AWS Repository URL for ECR"
-  value = module.pya.repository_url
+  value = module.web.repository_url
 }
