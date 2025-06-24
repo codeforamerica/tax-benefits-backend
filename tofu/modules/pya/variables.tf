646ee0f148d7e3d75c0bb206f6c7dbb7751822a3
Arin Choi <achoi@codeforamerica.org>
2025-06-24 09:27:15 -0700

    Remove variable no longe used

--- tofu/modules/pya/variables.tf
+++ tofu/modules/pya/variables.tf
@@ -29,9 +29,3 @@ variable "state_version_expiration" {
   description = "Age (in days) before non-current versions of the state file are expired."
   default     = 30
 }
-
-variable "kms_key_id" {
-  description = "ARN or Id of the AWS KMS key to be used to encrypt the secret values in the versions stored in this secret. If you need to reference a CMK in a different account, you can use only the key ARN. If you don't specify this value, then Secrets Manager defaults to using the AWS account's default KMS key (the one named `aws/secretsmanager`"
-  type        = string
-  default     = null
-}
