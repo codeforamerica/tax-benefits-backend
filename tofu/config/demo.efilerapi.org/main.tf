83df2093fde36ffceaedf968586692b148ee3b7a
Anisha Ramnani <anisha.ramnani@gmail.com>
2025-07-25 16:39:10 -0400

    broken VPC additon

--- tofu/config/demo.efilerapi.org/main.tf
+++ tofu/config/demo.efilerapi.org/main.tf
@@ -13,3 +13,22 @@ module "backend" {
   project     = "efiler-api"
   environment = "demo"
 }
+
+module "logging" {
+  source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.0"
+
+  project     = "efiler-api"
+  environment = "dev"
+}
+
+module "vpc" {
+  source = "github.com/codeforamerica/tofu-modules-aws-vpc?ref=1.1.1"
+
+  project        = "efiler-api"
+  environment    = "dev"
+  cidr           = "10.0.48.0/22"
+  logging_key_id = module.logging.kms_key_arn
+
+  private_subnets = ["10.0.50.0/26", "10.0.50.64/26", "10.0.50128/26"]
+  public_subnets  = ["10.0.48.0/26", "10.0.48.64/26", "10.0.48.128/26"]
+}
