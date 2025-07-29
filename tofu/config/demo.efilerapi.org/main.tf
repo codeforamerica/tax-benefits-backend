d72299c9276c39e24d6daf8a5cf0f572a66f8adb
Anisha Ramnani <aramnani@codeforamerica.org>
2025-07-29 18:25:27 -0400

    [TBE-191] E-Filer API host a Fargate Service (#69)
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- tofu/config/demo.efilerapi.org/main.tf
+++ tofu/config/demo.efilerapi.org/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "efiler-api-demo-tfstate"
-    key            = "demo.efilerapi.org"
+    key            = "demo.efiler-api.fileyourstatetaxes.org"
     region         = "us-east-1"
     dynamodb_table = "demo.tfstate"
   }
@@ -31,4 +31,21 @@ module "vpc" {
 
   private_subnets = ["10.0.50.0/26", "10.0.50.64/26", "10.0.50.128/26"]
   public_subnets  = ["10.0.48.0/26", "10.0.48.64/26", "10.0.48.128/26"]
+}
+
+module "web" {
+  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.4.0"
+
+  project       = "efiler-api"
+  project_short = "efiler-api"
+  environment   = "demo"
+  service       = "web"
+  service_short = "web"
+
+  domain          = "demo.efiler-api.fileyourstatetaxes.org"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  logging_key_id  = module.logging.kms_key_arn
+  container_port  = 4567
 }
