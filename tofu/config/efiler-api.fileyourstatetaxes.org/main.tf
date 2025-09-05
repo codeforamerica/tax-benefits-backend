a973b182ed8221a9b14c7c1c5d84838c4ee38b84
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2025-09-05 10:01:42 -0700

    TBE-200 efiler-api host a production application (#103)
    * add backend module
    * add required tf files
    * add efiler prod to deploy and plan gh workflow

---
+++ tofu/config/efiler-api.fileyourstatetaxes.org/main.tf
@@ -0,0 +1,25 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "efiler-api-production-tfstate"
+#     key            = "production.efiler-api.fileyourstatetaxes.org"
+#     region         = "us-east-1"
+#     dynamodb_table = "production.tfstate"
+#   }
+# }
+
+module "backend" {
+  source = "github.com/codeforamerica/tofu-modules-aws-backend?ref=1.1.1"
+
+  project     = "efiler-api"
+  environment = "production"
+}
+
+# module "efiler_api" {
+#   source = "../../modules/efiler_api"
+#
+#   environment         = "production"
+#   domain              = "production.efiler-api.fileyourstatetaxes.org"
+#   cidr                = "10.0.60.0/22"
+#   private_subnets     = ["10.0.62.0/26", "10.0.62.64/26", "10.0.62.128/2"]
+#   public_subnets      = ["10.0.60.0/26", "10.0.60.64/26", "10.0.60.128/26"]
+# }
