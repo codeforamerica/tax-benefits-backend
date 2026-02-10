52a0d6ecda2351860da4d0e8e697dec5b213ec0d
Drew Proebstel <dproebstel@codeforamerica.org>
2026-02-10 15:12:28 -0800

    create backend

--- tofu/config/gyraffe.getyourrefund.org/main.tf
+++ tofu/config/gyraffe.getyourrefund.org/main.tf
@@ -18,9 +18,9 @@ module "gyraffe" {
   source = "../../modules/gyraffe"
 
   environment     = "production"
-  domain          = "production.gyraffe.getyourrefund.org"
+  domain          = "gyraffe.getyourrefund.org"
   cidr            = "10.0.68.0/22"
   private_subnets = ["10.0.70.0/26", "10.0.70.64/26", "10.0.70.128/26"]
   public_subnets  = ["10.0.68.0/26", "10.0.68.64/26", "10.0.68.128/26"]
-  review_app      = "true"
+  review_app      = "false"
 }
