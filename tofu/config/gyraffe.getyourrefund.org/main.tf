42c91655557876e91f355f9764c9cc74d7adb0b6
Drew Proebstel <d.proebstel@gmail.com>
2026-02-11 15:18:07 -0800

    create gyraffe prod (#152)
    * create gyraffe prod
    ---------
    
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>

---
+++ tofu/config/gyraffe.getyourrefund.org/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket         = "gyraffe-production-tfstate"
+    key            = "production.gyraffe.getyourrefund.org"
+    region         = "us-east-1"
+    dynamodb_table = "production.tfstate"
+  }
+}
+
+module "backend" {
+  source = "github.com/codeforamerica/tofu-modules-aws-backend?ref=1.1.1"
+
+  project     = "gyraffe"
+  environment = "production"
+}
+
+module "gyraffe" {
+  source = "../../modules/gyraffe"
+
+  environment     = "production"
+  domain          = "gyraffe.getyourrefund.org"
+  cidr            = "10.0.92.0/22"
+  private_subnets = ["10.0.94.0/26", "10.0.94.64/26", "10.0.94.128/26"]
+  public_subnets  = ["10.0.92.0/26", "10.0.92.64/26", "10.0.92.128/26"]
+  review_app      = "false"
+}
---
+++ tofu/config/gyraffe.getyourrefund.org/outputs.tf
@@ -0,0 +1,24 @@
+output "log_bucket" {
+  description = "The Amazon S3 bucket to store log files."
+  value       = module.gyraffe.log_bucket
+}
+
+output "docker_push" {
+  description = "Display commands to push the Docker image to ECR."
+  value       = module.gyraffe.docker_push
+}
+
+output "repository_arn" {
+  description = "AWS Repository Amazon Resource Name (ARN) for ECR"
+  value       = module.gyraffe.repository_arn
+}
+
+output "repository_url" {
+  description = "AWS Repository URL for ECR"
+  value       = module.gyraffe.repository_url
+}
+
+output "submission_bundles_bucket_name" {
+  description = "S3 bucket where submission bundles are stored"
+  value       = module.gyraffe.submission_bundles_bucket_name
+}
