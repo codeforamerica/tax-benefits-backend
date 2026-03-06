bc64efd6b434c0e2cc2f5d0d64b37fac2f155adf
Rob DiCiuccio <rdiciuccio@codeforamerica.org>
2026-03-06 10:42:33 -0800

    chore: add Datadog monitors for Sensitive Data findings

--- tofu/config/datadog/main.tf
+++ tofu/config/datadog/main.tf
@@ -20,10 +20,24 @@ module "datadog" {
   ]
 }
 
+resource "datadog_integration_slack_channel" "security_alerts" {
+  account_name = "cfastaff"
+  channel_name = "#security-alerts"
+
+  display {
+    message  = true
+    notified = true
+    snapshot = false
+    tags     = true
+  }
+}
+
 module "sensitive_data_scanner" {
-  source = "github.com/codeforamerica/tofu-modules-datadog-sensitive-data-scanner?ref=1.0.1"
+  source = "github.com/codeforamerica/tofu-modules-datadog-sensitive-data-scanner?ref=rd/monitors"
 
   group_name   = "Production Environment Scanning"
   filter_query = "env:prod"
   product_list = ["logs", "apm"]
+  enable_monitors = true
+  notification_targets = ["@slack-cfastaff-security-alerts"]
 }
